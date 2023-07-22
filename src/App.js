d96a6aa3b8183f774719807169921e10a0424656
Om Patel <patelo10@dh2020pc28>
2023-07-22 15:17:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./Components/NavBar";
+import { Test } from "./Components/test";
+import { Banner } from "./Components/Banner";
+import { Skills } from "./Components/Skills";
+import { Projects } from "./Components/Projects";
+import { Contact } from "./Components/Contact";
+import { Footer } from "./Components/Footer";
+import { JudyBanner } from "./Components/JudyBanner";
+import AnimatedCursor from "react-animated-cursor";
+import AnimatedMouseCursor from './Components/AnimCursor';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <AnimatedCursor 
+        style={{ zIndex: 2000 }}
+        innerSize={8}
+        outerSize={30}
+        color='68, 238, 255'  // Teal color (adjust as needed)
+        outerAlpha={0.4}
+        innerScale={0.7}
+        outerScale={2}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
+      />
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+      {/* <JudyBanner /> */}
     </div>
   );
 }
